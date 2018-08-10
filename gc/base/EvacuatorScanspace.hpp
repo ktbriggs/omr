8f75380045663dfc03d9cfd3760cd4f60d4b30a8
Kim Briggs <briggs@ca.ibm.com>
2018-08-10 15:40:08 -0400

    WIP Evacuator: Apply fixes-for-evacuator-interleaved-frames
    Fixup scanspace and whitelist:
    
    Whitespace -> class with eycatchers
    Scanspace getObjectScanner -> getActiveObjectScanner
    
    Signed-off-by: Kim Briggs <briggs@ca.ibm.com>

--- gc/base/EvacuatorScanspace.hpp
+++ gc/base/EvacuatorScanspace.hpp
@@ -88,16 +88,16 @@ public:
 	 * Current or latent object scanner is instantiated in space reserved within containing scanspace. This
 	 * method should only be called when caller is committed to use the returned pointer for this purpose.
 	 *
-	 * To get current pointer to active object scanner, which may be NULL, use gertObjectScanner().
+	 * To get current pointer to active object scanner, which may be NULL, use getObjectScanner().
 	 *
-	 * @see getObjectScanner()
+	 * @see getActiveObjectScanner()
 	 */
 	MMINLINE GC_ObjectScannerState *getObjectScannerState() { _objectScanner = (GC_ObjectScanner *)&_objectScannerState; return &_objectScannerState; }
 
 	/**
-	 * Return pointer to instantiated object scanner, or NULL if object scanner not instantiated.
+	 * Return pointer to active object scanner, or NULL if object scanner not instantiated.
 	 */
-	MMINLINE GC_ObjectScanner *getObjectScanner() { return _objectScanner; }
+	MMINLINE GC_ObjectScanner *getActiveObjectScanner() { return _objectScanner; }
 
 	/**
 	 * Advance the scan pointer to next unscanned object and drop active object scanner.
